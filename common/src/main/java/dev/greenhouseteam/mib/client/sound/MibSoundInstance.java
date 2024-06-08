aeaaedce2421cc18157099b173177907f095ae7e
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-06-08 17:05:33 +1000

    Allow non looping instruments to play stop sound, update sound instance as to not be so reliant on players.

--- common/src/main/java/dev/greenhouseteam/mib/client/sound/MibSoundInstance.java
+++ common/src/main/java/dev/greenhouseteam/mib/client/sound/MibSoundInstance.java
@@ -12,24 +12,41 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Predicate;
 
 public class MibSoundInstance extends AbstractTickableSoundInstance {
+    @Nullable
     protected final Player player;
-    protected final ItemStack stack;
+    protected final Predicate<Player> stopPredicate;
     protected final ExtendedSound extendedSound;
     protected boolean hasPlayedLoop;
     protected boolean shouldPlayStopSound;
 
+    public MibSoundInstance(double x, double y, double z, SoundEvent sound,
+                            ExtendedSound extendedSound, SoundSource source,
+                            float volume, float pitch, boolean isLooping) {
+        this(null, x, y, z, p -> true, sound, extendedSound, source, volume, pitch, isLooping);
+    }
+
     public MibSoundInstance(Player player, ItemStack stack, SoundEvent sound,
                             ExtendedSound extendedSound, SoundSource source,
                             float volume, float pitch, boolean isLooping) {
+        this(player, player.getX(), player.getY(), player.getZ(), p -> !p.isUsingItem() || p.getUseItem() != stack, sound, extendedSound, source, volume, pitch, isLooping);
+    }
+
+    public MibSoundInstance(@Nullable Player player, double x, double y, double z, Predicate<Player> stopPredicate, SoundEvent sound,
+                            ExtendedSound extendedSound, SoundSource source,
+                            float volume, float pitch, boolean isLooping) {
         super(sound, source, SoundInstance.createUnseededRandom());
+        if (player != null)
+            ((PlayerAccess)player).mib$setCurrentSoundInstance(this);
         this.player = player;
-        this.x = player.getX();
-        this.y = player.getY();
-        this.z = player.getZ();
-        ((PlayerAccess)this.player).mib$setCurrentSoundInstance(this);
-        this.stack = stack;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.stopPredicate = stopPredicate;
         this.extendedSound = extendedSound;
         this.volume = volume;
         this.pitch = pitch;
@@ -44,18 +61,21 @@ public class MibSoundInstance extends AbstractTickableSoundInstance {
         if (!hasPlayedLoop && soundEngine.mib$getSoundDeleteTime().containsKey(this) && soundEngine.mib$getSoundDeleteTime().get(this) - 20 + extendedSound.durationBeforeLoop() <= soundEngine.mib$tickCount()) {
             hasPlayedLoop = true;
             shouldPlayStopSound = false;
-            Minecraft.getInstance().getSoundManager().queueTickingSound(new MibSoundInstance(player, stack, extendedSound.loopSound().orElse(extendedSound.startSound()).value(), extendedSound, source, volume, pitch, true));
+            Minecraft.getInstance().getSoundManager().queueTickingSound(new MibSoundInstance(player, x, y, z, stopPredicate, extendedSound.loopSound().orElse(extendedSound.startSound()).value(), extendedSound, source, volume, pitch, true));
         }
 
-        if (shouldPlayStopSound && (soundEngine.mib$getSoundDeleteTime().get(this) - 1 <= soundEngine.mib$tickCount() || !player.isUsingItem() || player.getUseItem() != stack || ((PlayerAccess)player).mib$getSoundInstance() != this)) {
+        if ((!hasPlayedLoop && soundEngine.mib$getSoundDeleteTime().get(this) - 1 <= soundEngine.mib$tickCount()) || hasPlayedLoop && shouldPlayStopSound && (soundEngine.mib$getSoundDeleteTime().get(this) - 1 <= soundEngine.mib$tickCount() || player != null && stopPredicate.test(player) || player != null && ((PlayerAccess)player).mib$getSoundInstance() != this)) {
             if (extendedSound.stopSound().isPresent())
                 Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(extendedSound.stopSound().get().value(), this.volume, this.pitch));
+            ((PlayerAccess)player).mib$setCurrentSoundInstance(null);
             stop();
             return;
         }
 
-        this.x = player.getX();
-        this.y = player.getY();
-        this.z = player.getZ();
+        if (player != null) {
+            this.x = player.getX();
+            this.y = player.getY();
+            this.z = player.getZ();
+        }
     }
 }
