5de46a38ddfe119c973240a7cb3b2b36b1938f0c
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-06-11 03:17:03 +1000

    Add item tags.

--- fabric/src/main/java/dev/greenhouseteam/mib/datagen/MibDatagen.java
+++ fabric/src/main/java/dev/greenhouseteam/mib/datagen/MibDatagen.java
@@ -3,15 +3,22 @@ package dev.greenhouseteam.mib.datagen;
 import dev.greenhouseteam.mib.data.KeyWithOctave;
 import dev.greenhouseteam.mib.data.MibSoundSet;
 import dev.greenhouseteam.mib.data.ExtendedSound;
+import dev.greenhouseteam.mib.registry.MibItems;
 import dev.greenhouseteam.mib.registry.MibSoundSets;
 import dev.greenhouseteam.mib.registry.MibSoundEvents;
+import dev.greenhouseteam.mib.registry.MibTags;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.valueproviders.ConstantFloat;
+import net.minecraft.world.item.Item;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -23,6 +30,7 @@ public class MibDatagen implements DataGeneratorEntrypoint {
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         FabricDataGenerator.Pack pack = generator.createPack();
         pack.addProvider(DynamicRegistryGenerator::new);
+        pack.addProvider(ItemTagProvider::new);
     }
 
     private static class DynamicRegistryGenerator extends FabricDynamicRegistryProvider {
@@ -41,6 +49,20 @@ public class MibDatagen implements DataGeneratorEntrypoint {
         @Override
         public @NotNull String getName() {
             return "Mib Dynamic Registries";
+        }
+    }
+
+    private static class ItemTagProvider extends FabricTagProvider.ItemTagProvider {
+        public ItemTagProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+            super(output, registriesFuture, null);
+        }
+
+        @Override
+        protected void addTags(HolderLookup.Provider wrapperLookup) {
+            getOrCreateTagBuilder(MibTags.ItemTags.INSTRUMENTS)
+                    .add(MibItems.FANTASY_TRUMPET)
+                    .add(MibItems.FLUTE)
+                    .add(MibItems.KEYBOARD);
         }
     }
 }
